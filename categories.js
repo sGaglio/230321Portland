24404217472680f409e454c129cc3744ed0eca34
sGaglio <128182525+sGaglio@users.noreply.github.com>
2023-03-21 23:35:27 -0500

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,27 @@
+// filter categories
+const categories = [
+  {
+    name: 'All',
+    filter: 'all',
+  },
+  {
+    name: 'Tue',
+    filter: 'tue',
+  },
+  {
+    name: 'Portrait',
+    filter: 'portrait',
+  },
+  {
+    name: 'Wedding',
+    filter: 'wedding',
+  },
+  {
+    name: 'Drone',
+    filter: 'drone',
+  },
+  {
+    name: 'Birds',
+    filter: 'birds',
+  },    
+];
